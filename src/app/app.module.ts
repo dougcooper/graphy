f7fcccc610fc2c7f32d4aa7f67553b638a2d946d
Doug Cooper <dougcooper@MiMac2.local>
2020-01-20 15:39:57 -0500

    add browser animations module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgxGraphModule } from '@swimlane/ngx-graph';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { NgxGraphModule } from '@swimlane/ngx-graph';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgxGraphModule
+    NgxGraphModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
